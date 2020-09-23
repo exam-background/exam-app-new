89e04cc72e5e407ea6346185849f30d38c80b755
SchemingCat <SchemingCat>
2020-09-23 15:47:04 +0800

    项目整合

---
+++ src/locations/location.js
@@ -0,0 +1,36 @@
+//考核
+var examsystemPath = 'http://localhost:8081/examsystem'
+
+const location ={
+  getStuById : examsystemPath + "/getStuById",
+  doLogin : examsystemPath + '/app/Student/doLogin',
+  courseForPage : examsystemPath + '/courseForPage',
+  // 查询所有专业分类
+  getProfessionalNoPage: examsystemPath + '/getProfessionalNoPage',
+  // 分页加载技术每日一练数据
+  getTechnologyDayExerciseByProfessionalId: examsystemPath + '/app/AppTechnologyDayExerciseController/getTechnologyDayExerciseSubmit',
+  // 分页加载技术每日一练数据
+  getJobDayExerciseByProfessid: examsystemPath + '/app/AppJobDayExerciseController/getJobDayExerciseByProfessid',
+  // 根据学生id查询所有试卷
+  getPapersUserByUserId: examsystemPath + '/app/PapersController/getPapersUserByUserId',
+  // 根据题目id查询就业每日一练
+  getJobDayExerciseById: examsystemPath + '/getJobDayExerciseById',
+  // 根据题目id查询师范每日一练
+  getTechnologyDayExerciseById: examsystemPath + '/getTechnologyDayExerciseById',
+  // 添加技术每日一练作答
+  addTechnologyDayExerciseSubmit: examsystemPath + '/app/TechnologyDayExerciseSubmitController/addTechnologyDayExerciseSubmit',
+  // 添加专业每日一练作答
+  addJobDayExerciseSubmit: examsystemPath + '/app/JobDayExerciesSubmitController/addJobDayExerciseSubmit',
+  // 根据用户id和类型查询试卷
+  getPapersByUserIdAndType: examsystemPath + '/app/PapersController/getPapersByUserIdAndType',
+  // 根据试卷id查询试卷
+  getPapersById: examsystemPath + '/app/PapersController/getPapersById',
+  // 根据试卷id查询试卷和学生作答
+  getPapersByIdStudent: examsystemPath + '/app/PapersController/getPapersByIdStudent',
+  // 添加学生作答
+  insertPapersUserResult: examsystemPath + '/app/AppPapersUserResultController/insertPapersUserResult',
+  // 根据学生id和类型查询已考试卷
+  getPapersByUserIdAndTypeFinish: examsystemPath + '/app/PapersController/getPapersByUserIdAndTypeFinish'
+}
+
+export default location
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import location from '@/locations/location';
+import commons from '@/locations/commons';
+import axios from 'axios';
+import qs from 'qs';
 
 import Vant from 'vant';
 import 'vant/lib/index.css';
@@ -9,6 +13,10 @@ import 'vant/lib/index.css';
 Vue.use(Vant);
 
 Vue.config.productionTip = false
+Vue.prototype.$location = location;
+Vue.prototype.$commons = commons;
+Vue.prototype.$axios = axios;
+Vue.prototype.$qs = qs;
 
 new Vue({
   router,
